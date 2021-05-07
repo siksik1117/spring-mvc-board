067c44ea88986958ea9168ee69b8eea3aa2eb8a3
siksik1117 <rla1gus1tlr@naver.com>
2021-05-07 14:54:59 +0900

    second commit

---
+++ board/src/main/java/com/board/domain/BoardVO.java
@@ -0,0 +1,51 @@
+package com.board.domain;
+
+import java.util.Date;
+
+public class BoardVO {
+	
+	private int id;
+	private String title;
+	private String content;
+	private String users;
+	private Date reg_date;
+	private int view_count;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getUsers() {
+		return users;
+	}
+	public void setUsers(String users) {
+		this.users = users;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	public int getView_count() {
+		return view_count;
+	}
+	public void setView_count(int view_count) {
+		this.view_count = view_count;
+	}
+
+}
